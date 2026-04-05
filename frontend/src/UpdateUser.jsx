3179d5aea51841876efe8b93614c37e1781768bb
mohdtajul <mohdtajul473@gmail.com>
2026-04-05 21:56:55 +0530

    added useState navigate fun api calls

--- frontend/src/UpdateUser.jsx
+++ frontend/src/UpdateUser.jsx
@@ -1,28 +1,78 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams, useNavigate } from "react-router-dom";
 
-function UpdateUser(){
-    return(
+
+function UpdateUser() {
+    const { id } = useParams();
+    const navigate = useNavigate();
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [age, setAge] = useState("");
+
+    useEffect(() => {
+        axios.get(`http://localhost:3001/getUser/${id}`)
+            .then(result => {
+                setName(result.data.name)
+                setEmail(result.data.email)
+                setAge(result.data.age)
+            })
+            .catch(err => console.log(err))
+    }, [])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault(); 
+
+        try {
+            await axios.put(`http://localhost:3001/updateUser/${id}`, {
+                name,
+                email,
+                age
+            });
+
+            navigate('/');
+
+        } catch (err) {
+            console.log(err);
+        }
+    }
+    return (
         <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
             <div className="w-50 bg-white rounded p-3">
-                <form >
+                <form onSubmit={handleSubmit}>
                     <h2>Update User</h2>
                     <div className="mb-2">
                         <label htmlFor="">Name</label>
-                        <input type="text" placeholder="Enter Name" className="form-control" />
+                        <input
+                            type="text"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            className="form-control"
+                        />
                     </div>
                     <div className="mb-2">
                         <label htmlFor="">Email</label>
-                        <input type="text" placeholder="Enter Email" className="form-control" />
+                        <input
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            className="form-control"
+                        />
                     </div>
                     <div className="mb-2">
                         <label htmlFor="">Age</label>
-                        <input type="text" placeholder="Enter Age" className="form-control" />
+                        <input
+                            type="number"
+                            value={age}
+                            onChange={(e) => setAge(e.target.value)}
+                            className="form-control"
+                        />
                     </div>
                     <button className="btn btn-success">Update</button>
                 </form>
 
             </div>
-           
+
         </div>
     );
 }
