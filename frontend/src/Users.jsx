7588bdd8af574d8fb8014b47bb1599b7c9b56ca5
mohdtajul <mohdtajul473@gmail.com>
2026-04-13 14:51:33 +0530

    Test CI/CD - frontend change

--- frontend/src/Users.jsx
+++ frontend/src/Users.jsx
@@ -27,7 +27,7 @@ function Users() {
     return (
         <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
             <div className="w-50 bg-white rounded p-3">
-                <Link to="/create" className='btn btn-success'>Add Users+</Link>
+                <Link to="/create" className='btn btn-success'>Add User+</Link>
                 <div style={{ maxHeight: "400px", overflowY: "auto" }}>
                     <table className="table">
                         <thead>
