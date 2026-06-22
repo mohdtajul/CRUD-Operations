0a16597cac4bfc792a79d53c8bdd29ee53aa06da
mohdtajul <mohdtajul473@gmail.com>
2026-06-22 17:50:59 +0530

    added comment for testing

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 const PORT = process.env.PORT || 3001;
 
 
-
+// mongoose connection
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB connected"))
     .catch(err => console.error(err));
