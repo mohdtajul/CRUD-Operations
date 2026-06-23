071e07e7d8635c221f9f3faeee4eec7af3d2ab86
mohdtajul <mohdtajul473@gmail.com>
2026-06-23 15:22:58 +0530

    testing complete automation

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 const PORT = process.env.PORT || 3001;
 
 
-// mongodDB connection 
+// mongo connection 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB connected"))
     .catch(err => console.error(err));
