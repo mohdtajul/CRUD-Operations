3fb26f4868d1d86a30dae5719955a7b4ab58499d
mohdtajul <mohdtajul473@gmail.com>
2026-06-23 16:09:08 +0530

    fix frontend env

--- backend/index.js
+++ backend/index.js
@@ -74,4 +74,5 @@ app.delete("/deleteUser/:id", async (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    console.log("this is for testing purpose");
 })
